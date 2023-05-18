695e24a95c5df2a5498c1a62dab5dcee1b813029
clown911026 <5568keb67595@gmail.com>
2023-05-18 21:28:47 +0800

    fix shoot ball problem

--- src/ball_pose/src/shoot_ball.cpp
+++ src/ball_pose/src/shoot_ball.cpp
@@ -14,7 +14,7 @@
 #include <time.h>
 #include <mutex>
 
-float x_origin,y_origin{};
+float x_origin{},y_origin{};
 float x_target{-7.f},y_target{};
 std::mutex m_;
 void shoot_ball(ros::NodeHandle& );
@@ -25,20 +25,20 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "apply_wrench");
   ros::NodeHandle nh;
-  ros::Subscriber model_states_sub_;
+  // ros::Subscriber model_states_sub_= nh.subscribe("/gazebo/model_states", 10, set_y_origin);
   ros::Publisher ball_predict_pos = nh.advertise<std_msgs::Float64>("ball_predict_pos", 1);
   std_msgs::Float64 pubMsg;
 
-  model_states_sub_ = nh.subscribe("/gazebo/model_states", 10, set_y_origin);
   while(1){
     //一輪射門要做的事
-    std::this_thread::sleep_for(std::chrono::seconds(1));
     ros::spinOnce();
 
     //射門
     shoot_ball(nh);
     pubMsg.data = x_target;
     ball_predict_pos.publish(pubMsg);
+
+    //block直到邊界條件 , 還沒寫好所以先用sleep
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
     //重置球的位置
@@ -65,39 +65,41 @@ void shoot_ball(ros::NodeHandle& nh){
   ROS_INFO("x : %f " ,velocity.first);
   ROS_INFO("y : %f ", velocity.second);
   srv.request.start_time = ros::Time::now();
-  srv.request.duration = ros::Duration(0.005f); 
+  srv.request.duration = ros::Duration(0.002f); 
   client.call(srv);
-}
-
-void set_y_origin(const gazebo_msgs::ModelStates::ConstPtr& msg) {
-  int model_index = -1;
-  for (int i = 0; i < msg->name.size(); i++) {
-    if (msg->name[i] == "unit_sphere") {
-      model_index = i;
-      break;
-    }
-  }
-  if (model_index >= 0) 
-    x_origin=msg->pose[model_index].position.x;
-    y_origin=msg->pose[model_index].position.y;
-  ROS_INFO("Model position: y_origin=%f",y_origin);
 }
 
 
 void set_ball_pos_to_origin(ros::NodeHandle& nh){
 
-  ros::ServiceClient client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
-  gazebo_msgs::SetModelState setmodelstate;
-  gazebo_msgs::ModelState modelstate;
   geometry_msgs::Twist model_twist;
+  geometry_msgs::Pose start_pose;
+  x_origin=0.f;
+  y_origin=rand()%8-4.f;
+  start_pose.position.x = x_origin;
+  start_pose.position.y = y_origin;
+  start_pose.position.z = 0.0;
+  start_pose.orientation.x = 0.0;
+  start_pose.orientation.y = 0.0;
+  start_pose.orientation.z = 0.0;
+  start_pose.orientation.w = 0.0;
+
+  geometry_msgs::Twist start_twist;
+  start_twist.linear.x = 0.0;
+  start_twist.linear.y = 0.0;
+  start_twist.linear.z = 0.0;
+  start_twist.angular.x = 0.0;
+  start_twist.angular.y = 0.0;
+  start_twist.angular.z = 0.0;
+
+  gazebo_msgs::ModelState modelstate;
   modelstate.model_name = "unit_sphere";
   modelstate.reference_frame = "world";
-  srand(time(NULL));
-  model_twist.linear.x = 0.f;
-  model_twist.linear.y = rand()%16 - 8.f;
-  y_origin=model_twist.linear.y;
-  model_twist.linear.z = 0.f;
-  modelstate.twist = model_twist;
+  modelstate.pose = start_pose;
+  modelstate.twist = start_twist;
+
+  ros::ServiceClient client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+  gazebo_msgs::SetModelState setmodelstate;
   setmodelstate.request.model_state = modelstate;
   client.call(setmodelstate);
 }
